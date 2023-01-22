abff1c0439fbf6f48b0060d99cb8c34ee00c1e0c
salimmujawar <salim685@gmail.com>
2023-01-22 07:25:39 +0400

    while loop practice sumDigits

--- src/ControlFlow/WhileLoop/WhileLoop.java
+++ src/ControlFlow/WhileLoop/WhileLoop.java
@@ -6,6 +6,12 @@ public class WhileLoop {
         int finishNumber = 20;
         int evenCount = 0;
         int oddCount = 0;
+
+        System.out.println("For the digit 123, total sum is: "+ sumDigits(123));
+        System.out.println("For the digit -123, total sum is: "+ sumDigits(-123));
+        System.out.println("For the digit 4, total sum is: "+ sumDigits(4));
+        System.out.println("For the digit 32123, total sum is: "+ sumDigits(32123));
+
         while (number <= finishNumber) {
             number++;
             if (!isEvenNumber(number)) {
@@ -24,5 +30,22 @@ public class WhileLoop {
 
     public static boolean isEvenNumber(int num) {
         return (num%2 == 0) ? true : false;
+    }
+
+    public static int sumDigits(int number) {
+        int digit = number;
+        int sumOfDigits = 0;
+        if (number < 0) {
+            return -1;
+        }
+
+        while(true){
+            sumOfDigits += number%10;
+            number = number/10;
+            if (number == 0) {
+                break;
+            }
+        }
+        return sumOfDigits;
     }
 }
